243b772524269db26566cff8b7b5e4925b29bf5a
Roberto Zanchi <r.zanchi@gmail.com>
2020-02-03 23:21:53 -0500

    Add ex 3 of Basics

---
+++ 1_Basics/Basics_ex3.js
@@ -0,0 +1,19 @@
+'true'; // Type: String
+console.log(typeof 'true');
+
+false; // Type: Bool
+console.log(typeof false);
+
+1.5; // Type: Number (float)
+console.log(typeof 1.5);
+
+2; // Type: Number (int)
+console.log(typeof 2);
+
+undefined; // Type: Undefined
+console.log(typeof undefined);
+
+{
+	foo: 'bar';
+} // Object
+console.log(typeof { foo: 'bar' });
