d1c705ca4893a6c1a9bc5ba7d994ee58b428f3e0
Roberto Zanchi <r.zanchi@gmail.com>
2020-02-04 21:55:50 -0500

    Add all ex of Input/Output

---
+++ 3_I-O/I-O_ex3.js
@@ -0,0 +1,6 @@
+let rlSync = require('readline-sync');
+let age = Number(rlSync.question('How old are you? '));
+console.log(`You are ${age} years old.`);
+console.log(`In 10 years, you will be ${age + 10} years old.`);
+console.log(`In 20 years, you will be ${age + 20} years old.`);
+console.log(`In 30 years, you will be ${age + 30} years old.`);
