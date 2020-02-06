5dc85ad7c81495240b006c0c025710ffc92233a0
Roberto Zanchi <r.zanchi@gmail.com>
2020-02-05 21:31:23 -0500

    Add examples from Functions

---
+++ 4_Functions/greeting.js
@@ -0,0 +1,12 @@
+// // greeting.js
+// let greetingMessage = 'Good Morning!';
+// console.log(greetingMessage);
+
+// greeting.js
+function greetPeople() {
+	let greetingMessage = 'Good Morning';
+	console.log(greetingMessage);
+}
+
+greetPeople();
+console.log(greetingMessage); // raises ReferenceError
