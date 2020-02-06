fb053113845680c1eb2ae1f93ce5208e5a30f104
Roberto Zanchi <r.zanchi@gmail.com>
2020-02-06 00:21:36 -0500

    Add ex 5 and 6 of Functions

---
+++ 4_Functions/Functions_ex5.js
@@ -0,0 +1,11 @@
+// What does the following code log to the console?
+
+function scream(words) {
+	words = words + '!!!!';
+	return;
+	console.log(words);
+}
+
+scream('Yipeee');
+
+/* A. The function does not return or log any value. */
---
+++ 4_Functions/Functions_ex6.js
@@ -0,0 +1,9 @@
+// What does the following code log to the console?
+
+function scream(words) {
+	return words + '!!!!';
+}
+
+scream('Yipeee');
+
+// The function returns 'Yipeee!!!!' but logs nothing to the console.
